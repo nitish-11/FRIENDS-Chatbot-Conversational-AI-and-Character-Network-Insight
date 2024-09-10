581f94845607168e4d08652d7e97beab70b7d3a5
Nitish Gupta <nitishgupta8971@gmail.com>
2024-09-10 21:44:58 +0530

    updation

--- crawler/jutsu_crawler.py
+++ crawler/jutsu_crawler.py
@@ -1,20 +1,19 @@
 import scrapy
 from bs4 import BeautifulSoup
 
-
 class BlogSpider(scrapy.Spider):
     name = 'narutospider'
     start_urls = ['https://naruto.fandom.com/wiki/Special:BrowseData/Jutsu?limit=250&offset=0&_cat=Jutsu']
 
     def parse(self, response):
         for href in response.css('.smw-columnlist-container')[0].css("a::attr(href)").extract():
-            extracted_data = scrapy.Request("https://naruto.fandom.com" + href,
-                                            callback=self.parse_jutsu)
+            extracted_data = scrapy.Request("https://naruto.fandom.com"+href,
+                           callback=self.parse_jutsu)
             yield extracted_data
 
         for next_page in response.css('a.mw-nextlink'):
             yield response.follow(next_page, self.parse)
-
+    
     def parse_jutsu(self, response):
         jutsu_name = response.css("span.mw-page-title-main::text").extract()[0]
         jutsu_name = jutsu_name.strip()
@@ -24,11 +23,11 @@ class BlogSpider(scrapy.Spider):
 
         soup = BeautifulSoup(div_html).find('div')
 
-        jutsu_type = ""
+        jutsu_type=""
         if soup.find('aside'):
             aside = soup.find('aside')
 
-            for cell in aside.find_all('div', {'class': 'pi-data'}):
+            for cell in aside.find_all('div',{'class':'pi-data'}):
                 if cell.find('h3'):
                     cell_name = cell.find('h3').text.strip()
                     if cell_name == "Classification":
@@ -39,8 +38,8 @@ class BlogSpider(scrapy.Spider):
         jutsu_description = soup.text.strip()
         jutsu_description = jutsu_description.split('Trivia')[0].strip()
 
-        return dict(
-            jutsu_name=jutsu_name,
-            jutsu_type=jutsu_type,
-            jutsu_description=jutsu_description
+        return dict (
+            jutsu_name = jutsu_name,
+            jutsu_type = jutsu_type,
+            jutsu_description = jutsu_description
         )
