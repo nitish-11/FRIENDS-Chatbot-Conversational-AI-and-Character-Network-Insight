c942b668e5cae9ea5f6c7dd18b3839b01393384a
Nitish Gupta <nitishgupta8971@gmail.com>
2024-09-26 00:14:52 +0530

    changes:for rachel model

--- friends_chracter_new/friend_character_chatbox.py
+++ friends_chracter_new/friend_character_chatbox.py
@@ -54,7 +54,7 @@ class CharacterChatBot():
     def chat(self, message, history):
         messages = []
         # Add the system ptomp 
-        messages.append({"role":"system","content": """" Your are Ross from the Friends Tv Show". Your responses should reflect his personality and speech patterns \n"""})
+        messages.append({"role":"system","content": """" Your are Rachel from the Friends Tv Show". Your responses should reflect his personality and speech patterns \n"""})
 
         for message_and_respnse in history:
             messages.append({"role":"user","content":message_and_respnse[0]})
@@ -237,12 +237,12 @@ class CharacterChatBot():
         friends_transcript_df['Dialogue'] = friends_transcript_df['Dialogue'].apply(remove_paranthesis)
         friends_transcript_df['number_of_words'] = friends_transcript_df['Dialogue'].str.strip().str.split(" ")
         friends_transcript_df['number_of_words'] = friends_transcript_df['number_of_words'].apply(lambda x: len(x))
-        friends_transcript_df['Ross_response_flag'] = 0
-        friends_transcript_df.loc[(friends_transcript_df['Speaker'] == 'Ross') & (friends_transcript_df['number_of_words'] > 5), 'Ross_response_flag'] = 1
+        friends_transcript_df['Rachel_response_flag'] = 0
+        friends_transcript_df.loc[(friends_transcript_df['Speaker'] == 'Rachel') & (friends_transcript_df['number_of_words'] > 5), 'Rachel_response_flag'] = 1
 
-        indexes_to_take = list(friends_transcript_df[(friends_transcript_df['Ross_response_flag'] == 1) & (friends_transcript_df.index > 0)].index)
+        indexes_to_take = list(friends_transcript_df[(friends_transcript_df['Rachel_response_flag'] == 1) & (friends_transcript_df.index > 0)].index)
 
-        system_promt = """" Your are Ross from the Friends Tv Show". Your responses should reflect his personality and speech patterns \n"""
+        system_promt = """" Your are Rachel from the Friends Tv Show". Your responses should reflect his personality and speech patterns \n"""
 
         prompts = []
         for ind in indexes_to_take:
