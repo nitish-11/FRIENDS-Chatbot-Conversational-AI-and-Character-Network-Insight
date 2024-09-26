e3602457b183bcbc054b392f9eb7f6d35194c9da
Nitish Gupta <nitishgupta8971@gmail.com>
2024-09-26 22:38:32 +0530

    drop-down option adding

--- deploy.py
+++ deploy.py
@@ -75,6 +75,70 @@
 
 
 
+# import gradio as gr
+# from friends_chracter_new.friend_character_chatbox import CharacterChatBot
+# import os
+
+# # Mapping of character names to their corresponding model paths
+# character_models = {
+#     "Ross": "nitish-11/friends_Ross_trained2_Llama-3-8B",
+#     "Rachel": "nitish-11/friends_Rachel_trained_Llama-3-8B",
+#     "Chandler": "nitish-11/friends_Chandler_trained_Llama-3-8B",
+#     "Monica": "nitish-11/friends_Monica_trained_Llama-3-8B",
+#     "Joey": "nitish-11/friends_Joey_trained_Llama-3-8B",
+#     "Phoebe" : "nitish-11/friends_Phoebe_trained_Llama-3-8B"
+# }
+
+
+
+
+# def chat_with_character_chatbot(character, message, history):
+#     character_chatbot = CharacterChatBot(character_models[character],
+#                                          huggingface_token=os.getenv('huggingface_token'))
+#     output = character_chatbot.chat(message, history)
+#     output = output['content'].strip()
+#     return output
+
+# def main():
+#     with gr.Blocks() as iface:
+#         # Full-screen layout
+#         with gr.Row(elem_id="header_row", equal_height=True):
+#             gr.HTML("""
+#                 <div style="text-align: center; padding: 20px; background-color: #f5f5f5; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
+#                     <h1 style="font-family: 'Arial', sans-serif; color: #333;">Friends Character Chatbot</h1>
+#                     <p style="font-size: 18px; color: #555;">Chat with your favorite Friends character</p>
+#                 </div>
+#             """)
+
+#         # Character Selection and Chatbot Section
+#         with gr.Row(elem_id="chat_row", equal_height=True):
+#             with gr.Column():
+#                 # Dropdown for character selection
+#                 character_dropdown = gr.Dropdown(label="Choose a character", 
+#                                                   choices=list(character_models.keys()), 
+#                                                   value="Ross")
+                
+#                 # Chat Interface
+#                 chat_interface = gr.ChatInterface(fn=lambda message, history: chat_with_character_chatbot(character_dropdown.value, message, history))
+#                                                    #height=600,  # Set the chat window height
+#                                                    #container=True)
+
+#         # Link dropdown change to chat interface
+#         character_dropdown.change(fn=lambda _: chat_interface.clear(), inputs=character_dropdown, outputs=chat_interface)
+
+#     iface.launch(share=True)
+
+# if __name__ == '__main__':
+#     main()
+
+
+
+
+#----------------------------------------------------------------------------------------------------------------
+
+
+
+
 import gradio as gr
 from friends_chracter_new.friend_character_chatbox import CharacterChatBot
 import os
@@ -86,11 +150,8 @@ character_models = {
     "Chandler": "nitish-11/friends_Chandler_trained_Llama-3-8B",
     "Monica": "nitish-11/friends_Monica_trained_Llama-3-8B",
     "Joey": "nitish-11/friends_Joey_trained_Llama-3-8B",
-    "Phoebe" : "nitish-11/friends_Phoebe_trained_Llama-3-8B"
+    "Phoebe": "nitish-11/friends_Phoebe_trained_Llama-3-8B"
 }
-
-
-
 
 def chat_with_character_chatbot(character, message, history):
     character_chatbot = CharacterChatBot(character_models[character],
@@ -103,12 +164,10 @@ def main():
     with gr.Blocks() as iface:
         # Full-screen layout
         with gr.Row(elem_id="header_row", equal_height=True):
-            gr.HTML("""
-                <div style="text-align: center; padding: 20px; background-color: #f5f5f5; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
-                    <h1 style="font-family: 'Arial', sans-serif; color: #333;">Friends Character Chatbot</h1>
-                    <p style="font-size: 18px; color: #555;">Chat with your favorite Friends character</p>
-                </div>
-            """)
+            gr.HTML("""<div style="text-align: center; padding: 20px; background-color: #f5f5f5; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
+                        <h1 style="font-family: 'Arial', sans-serif; color: #333;">Friends Character Chatbot</h1>
+                        <p style="font-size: 18px; color: #555;">Chat with your favorite Friends character</p>
+                        </div>""")
 
         # Character Selection and Chatbot Section
         with gr.Row(elem_id="chat_row", equal_height=True):
@@ -120,8 +179,6 @@ def main():
                 
                 # Chat Interface
                 chat_interface = gr.ChatInterface(fn=lambda message, history: chat_with_character_chatbot(character_dropdown.value, message, history))
-                                                   #height=600,  # Set the chat window height
-                                                   #container=True)
 
         # Link dropdown change to chat interface
         character_dropdown.change(fn=lambda _: chat_interface.clear(), inputs=character_dropdown, outputs=chat_interface)
